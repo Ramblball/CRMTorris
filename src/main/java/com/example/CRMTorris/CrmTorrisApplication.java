284bf6f0fc98342d88a56f296d870613039e0cb2
Maxim <lostrandg@gmail.com>
2021-08-17 22:50:05 +0500

    init commit

---
+++ src/main/java/com/example/CRMTorris/CrmTorrisApplication.java
@@ -0,0 +1,12 @@
+package com.example.CRMTorris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmTorrisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmTorrisApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/CRMTorris/CrmTorrisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CRMTorris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmTorrisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
